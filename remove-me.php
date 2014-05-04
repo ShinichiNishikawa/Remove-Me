06480b154e67100662c1b2b8b220d65d1a4e7d80
Shinichi Nishikawa <shinichi.nishikawa@gmail.com>
2014-05-04 17:02:53 +0700

    English.

--- remove-me.php
+++ remove-me.php
@@ -11,65 +11,68 @@ Author URI: http://nskw-style.com
 add_action( 'init', 'remove_me' );
 function remove_me() {
 	
-	// マルチサイトはそのまま返す
+	// return multisite
 	if ( is_multisite() )
 		return;
 	
-	// ログインしていないのはそのまま返す
+	// return if user is not logged in
 	if ( !is_user_logged_in() )
 		return;
 
-	// POSTでremove_meが渡されていないならそのまま返す
+	// return if no $_POST['remove_me']
 	if ( !isset( $_POST['remove_me'] ) || $_POST['remove_me'] !== 'sure' )
 		return;
 
-	// 以降、シングルインストールで、
-	// ログインユーザーによって$_POST['remove_me']が投げられた場合
+	// now all the request is posted on single install WP by
+	// a logged in user.
 
-	// 他のユーザーを管理画面で削除することができる＝管理者の場合
+	// if current user can delete users, it can be the only admin
+	// so return.
 	if ( current_user_can( 'delete_users' ) )
-		wp_die( '管理者は自分を削除できません。管理画面からどうぞ' );
+		wp_die( 'Admins should\'t delete theirselves by this plugin.' );
 
-	// $_POST['remove_me'] があるのに送信者のユーザIDがないのはダメ
+	// check the senders ID
 	if ( !isset( $_POST['senders_id'] ) || '' == $_POST['senders_id'] )
-		wp_die( '不正な操作が行われました。' );
+		wp_die( 'Something\'s wrong.' );
 
-	// 整数かどうか確認する
+	// Check if the id of the user who requested it is a int
 	$senders_id = intval( $_POST['senders_id'], 10 );
 	if ( ! $senders_id )
-		wp_die( '不正な操作が行われました。' );
+		wp_die( 'Something\'s wrong.' );
 
-	// 表示中のログインユーザーIDと送られてきたユーザーIDを比較
+	// compare senders matches the logged in user
 	$current_user = wp_get_current_user();
 	if ( $current_user->ID != $senders_id )
-		wp_die( '不正な操作が行われました。' );
+		wp_die( 'Something\'s wrong.' );
 	
 	if ( !check_admin_referer( 'randtext', '_my_nonce' ) )
 		return;
 
-	// 削除する
+	// Finally, delete.
 	require_once( ABSPATH.'wp-admin/includes/user.php' );
 	$deleted = wp_delete_user( $current_user->ID, 1 );
 	
 	if ( $deleted ) {
-		wp_die( '退会処理を実行しました。' );
+		wp_die( 'Deleted.' );
 	} else {
-		wp_die( '失敗しました。' );
+		wp_die( 'Failed.' );
 	}
 		
 }
 
-// 自分を削除するためのボタンを表示する
+// display delete form by calling this function
 function display_remove_me_form() {
 
-	// マルチサイトは返す
+	// return multisite
 	if ( is_multisite() )
 		return;
 	
+	// return if user is not logged in
 	if ( ! is_user_logged_in() )
-		wp_die( 'この画面はログインユーザー専用の画面です。<br><a href="' . home_url('/') . '">トップページへ戻る</a>' );
+		wp_die( 'You need to login to see this.<br><a href="' . home_url('/') . '">Go to home page</a>.' );
 	
-	// 他のユーザーを管理画面で削除することができる＝管理者の場合はそのまま返す
+	// if current user can delete users, it can be the only admin
+	// so return.
 	if ( current_user_can( 'delete_users' ) )
 		return;
 
@@ -77,12 +80,12 @@ function display_remove_me_form() {
 	$senders_id = $current_user->ID;
 	?>
 <form id="remove-me" method="post" action="">
-	<label for="remove_me">本当に削除しますか？</label>
+	<label for="remove_me">Do you really want to delete your account? By clicking the button, WordPress will delete all of your information and your posts.</label>
 	<input type="checkbox" id="remove_me" name="remove_me" value="sure" />
 	<input type="hidden" id="senders_id" name="senders_id" value="<?php echo (int)$senders_id; ?>">
 	<?php wp_nonce_field( 'randtext', '_my_nonce' ); ?>
 	<br />
-	<input type="submit" value="自分のアカウントを削除する" />
+	<input type="submit" value="Delete my account." />
 </form>
 	<?php
 	
